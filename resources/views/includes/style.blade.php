b4f45df981659b6e50cc34d61f830f2bf0e9bd86
caturyd <caturytd04@gmail.com>
2026-01-31 18:50:51 +0700

    update fix cssjss

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -3,11 +3,11 @@
 <meta name="author" content="Wahyu Mart">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 
-{{-- <link rel="shortcut icon" href="{{ asset('assets/img/icons/icon-48x48.png') }}" />
-<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet"> --}}
+<link rel="shortcut icon" href="{{ asset('assets/img/icons/icon-48x48.png') }}" />
+<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
 
-<link rel="shortcut icon" href="{{ asset('img/icons/icon-48x48.png') }}" />
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+{{-- <link rel="shortcut icon" href="{{ asset('img/icons/icon-48x48.png') }}" />
+<link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
